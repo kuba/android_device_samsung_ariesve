0b5093fd8ca309859ca8ce8e2dba27aff2b5e9f7
Tiziano Carotti <tizianoc@fastwebnet.it>
2013-01-28 22:38:51 +0100

    Adjustments from arco ics-3.0 branch! (2)

--- ariesve.mk
+++ ariesve.mk
@@ -119,6 +119,7 @@ PRODUCT_PACKAGES += \
     gralloc.msm7x30 \
     hwcomposer.msm7x30 \
     gps.msm7x30 \
+    power.msm7x30 \
     audio.primary.msm7x30 \
     audio_policy.msm7x30 \
     audio_policy.conf \
