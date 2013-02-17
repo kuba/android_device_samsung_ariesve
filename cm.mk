0d2b808a830e0e838f81004c97a33d70d974b026
Tiziano Carotti <tizianoc@fastwebnet.it>
2013-02-17 10:44:03 +0100

    beta2 bump!

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(LOCAL_PATH)/full_ariesve.mk)
 
 PRODUCT_RELEASE_NAME := ariesve
 
-CM_BUILDTYPE := ivendor-beta1
-CM_NUMERICAL_VERSION := 0.1.0
+CM_BUILDTYPE := ivendor-beta2
+CM_NUMERICAL_VERSION := 0.2.0
 
 $(call inherit-product, $(LOCAL_PATH)/ota-update-center.mk)
 
